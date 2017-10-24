d05739aad94149c07ecc4bd7030c9e4345780f3d
zhilvmac <zhilvmac@zhilvmacdeMacBook-Pro.local>
2017-10-24 10:05:56 +0800

    no message

---
+++ Example/Pods/DSZKit/DSZBase/DSZBaseTableViewController.h
@@ -0,0 +1,85 @@
+//
+//  DSZBaseTableViewController.h
+//  Demo
+//
+//  Created by zhilvmac on 2017/10/23.
+//  Copyright © 2017年 zjwist. All rights reserved.
+//
+
+#import "DSZBaseViewController.h"
+#import "DSZBaseTableView.h"
+@interface DSZBaseTableViewController : DSZBaseViewController<UITableViewDelegate,UITableViewDataSource>
+@property (nonatomic,strong)DSZBaseTableView *tableView;
+@property(nonatomic,assign)NSInteger pageIndex;
+@property(nonatomic,strong)NSMutableArray *dataSource;//数据源
+@property(nonatomic,assign)BOOL hasNextPage;//是否有下一页
+
+
+/**
+ 设置tableView的样式
+ */
+@property(nonatomic,assign)UITableViewStyle tableViewStyle;
+
+/**
+ 下啦刷新
+ */
+- (void)addHeaderRefresh;
+
+/**
+ 上提加载
+ */
+- (void)addFootRefresh;
+
+
+/**
+ 结束刷新
+ */
+- (void)endRefresh;
+
+/**
+ 刷新回调
+ */
+- (void)dszGetNetWork;
+
+/**
+ 刷新tableView
+ */
+- (void)reloadTableView;
+
+
+/**
+ 注册cell
+ */
+- (void)regiserCell;
+/**
+ 注册cell
+
+ @param classNames 需要注册的cell类名称数组
+ */
+- (void)regiserCellWithClassName:(NSArray *)classNames;
+
+/**
+ 注册cell
+
+ @param classNames 需要注册的cell的xib类名称
+ */
+- (void)regiserCellWithNibClassName:(NSArray *)classNames;
+
+
+/**
+ *  替换无数据时显示的View
+ *
+ *  @param view 自定义View
+ */
+- (void)changeEmptyView:(UIView *)view;
+
+/**
+ *  设置无数据时显示的Viewframe
+ */
+- (void)resetEmptyViewFrame:(CGRect)frame;
+
+/**
+ *  设置无数据时显示的Viewframe
+ */
+- (void)setEmptyViewHidden;
+@end
---
+++ Example/Pods/DSZKit/DSZBase/DSZBaseTableViewController.m
@@ -0,0 +1,327 @@
+//
+//  DSZBaseTableViewController.m
+//  Demo
+//
+//  Created by zhilvmac on 2017/10/23.
+//  Copyright © 2017年 zjwist. All rights reserved.
+//
+
+#import "DSZBaseTableViewController.h"
+#import <MJRefresh.h>
+#import <DSZKitMacro.h>
+#import "UIView+DSZExt.h"
+#import "UITableView+DSZExt.h"
+@interface DSZBaseTableViewController ()
+
+@property (nonatomic, strong) UIView *emptyView;
+@property (nonatomic, strong) UIView *emptyFootView;
+
+@end
+
+@implementation DSZBaseTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self loadTableView];
+    [self regiserCell];
+    self.view.backgroundColor = [UIColor groupTableViewBackgroundColor];
+    self.tableView.backgroundColor = [UIColor redColor];
+}
+
+
+
+/**
+ 懒加载
+
+ @return tableView默认为Plain
+ */
+- (DSZBaseTableView *)tableView{
+
+    if (!_tableView) {
+        if (self.tableViewStyle == UITableViewStylePlain) {
+            _tableView = [[DSZBaseTableView alloc]initWithFrame:self.view.bounds style:(UITableViewStylePlain)];
+        }else{
+            _tableView = [[DSZBaseTableView alloc]initWithFrame:self.view.bounds style:(UITableViewStyleGrouped)];
+            _tableView.tableHeaderView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 0.001, 0.001)];
+        }
+    }
+    return _tableView;
+}
+
+
+/* 目前基本可以兼容有表头的tableView
+ * 无表头的话  请在所使用的类里加上self.emptyView.frame = self.tableView.frame;
+ */
+- (UIView *)emptyView {
+    if (!_emptyView) {
+        
+        CGFloat posY = self.tableView.tableHeaderView.frame.size.height;
+        CGFloat height = 0.0;
+        
+        //        if (self.tableView.frame.origin.y == 0.0 && posY == 0.0) {
+        //            posY = 64.0;
+        //        } else {
+        //
+        //        }
+        
+        if (self.tableView.tableHeaderView.frame.size.height != 0.0) {
+            height = self.tableView.frame.size.height - self.tableView.tableHeaderView.frame.size.height;
+        } else {
+            height = self.tableView.frame.size.height;
+        }
+        
+        _emptyView = [[UIView alloc]initWithFrame:CGRectMake(0.0, posY, self.tableView.frame.size.width, height)];
+        
+        UIImage *image = DSZBaseImageName(@"list_empty");
+        UIImageView *imageView = [[UIImageView alloc] initWithImage:image];
+        
+        CGFloat centerY = 0.0;
+        if (self.tableView.tableHeaderView.frame.size.height != 0.0) {
+            centerY = _emptyView.center.y - 120.0;
+        } else {
+            centerY = _emptyView.center.y - 90.0;
+        }
+        
+        imageView.size = CGSizeMake(188, 165);
+        imageView.center = CGPointMake(_emptyView.center.x, centerY);
+        // imageView.center = view.center;
+        _emptyView.hidden = YES;
+        _emptyView.backgroundColor = [UIColor clearColor];
+        [_emptyView addSubview:imageView];
+        
+        UILabel *emptyLbl = [[UILabel alloc] initWithFrame:CGRectMake(16, imageView.frame.origin.y + imageView.frame.size.height + 8, DSZScreenWidth-32, 30)];
+        
+       
+        
+        emptyLbl.text = @"暂无数据";
+        emptyLbl.textAlignment = NSTextAlignmentCenter;
+        emptyLbl.font = [UIFont systemFontOfSize:16];
+        emptyLbl.textColor = UIColorFrom16RGB(0x787878);
+        [_emptyView addSubview:emptyLbl];
+        
+    }
+    
+    return _emptyView;
+}
+
+- (UIView *)emptyFootView {
+    
+    if (!_emptyFootView) {
+        _emptyFootView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, DSZScreenWidth, 30)];
+        
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, DSZScreenWidth, 30)];
+        label.textAlignment = NSTextAlignmentCenter;
+        label.text = @"我是有底线的";
+        [_emptyFootView addSubview:label];
+        
+    }
+    
+    
+    
+    return _emptyFootView;
+    
+    
+    
+}
+
+
+
+-(void)setTableViewStyle:(UITableViewStyle)tableViewStyle{
+    _tableViewStyle = tableViewStyle;
+}
+
+
+- (void)loadTableView {
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.tableView.frame = CGRectMake(0, NavigationBarAndStatusHeight, DSZScreenWidth, DSZViewHeight);
+    [self.view addSubview:self.tableView];
+    self.tableView.showsVerticalScrollIndicator = NO;
+    self.tableView.showsHorizontalScrollIndicator = NO;
+}
+
+
+
+- (void)regiserCell {
+    
+    DAssert(0)
+    NSLog(@"子类必须重写");
+    
+}
+
+
+/**
+ 注册cell
+ 
+ @param classNames 需要注册的cell名称数组
+ */
+- (void)regiserCellWithClassName:(NSArray *)classNames {
+    for (NSString *identify in classNames) {
+        [self.tableView registerClass:[NSClassFromString(identify) class] forCellReuseIdentifier:identify];
+    }
+}
+- (void)regiserCellWithNibClassName:(NSArray *)classNames {
+    for (NSString *identify in classNames) {
+        [self.tableView registerNib:[UINib nibWithNibName:identify bundle:nil] forCellReuseIdentifier:identify];
+    }
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - tableView代理
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return self.dataSource.count >0?1:0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.dataSource.count;
+}
+
+//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+//
+//}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 44;
+}
+
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+}
+
+- (void)back {
+    [super back];
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    
+    UILabel *label = [UILabel new];
+    label.text = @"3fsdfsdfsdf";
+    label.backgroundColor = [UIColor greenColor];
+    return label;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section {
+    return [UIView new];
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
+{
+    return 0.001;
+}
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 20;
+}
+
+- (void)addHeaderRefresh {
+    self.tableView.mj_header = [MJRefreshNormalHeader headerWithRefreshingBlock:^{
+        [self headerRereshing];
+    }];
+    [self.tableView.mj_header beginRefreshing];
+    
+}
+
+- (void)addFootRefresh {
+    self.tableView.mj_footer = [MJRefreshBackNormalFooter footerWithRefreshingBlock:^{
+        [self footerRereshing];
+    }];
+}
+
+- (void)endRefresh
+{
+    [self.tableView.mj_header endRefreshing];
+    [self.tableView.mj_footer endRefreshing];
+}
+- (void)headerRereshing
+{
+    _pageIndex = 1;
+    // 刷新表格UI
+    [self dszGetNetWork];
+    
+}
+
+- (void)footerRereshing
+{
+    _pageIndex ++;
+    [self dszGetNetWork];
+}
+
+
+- (void)reloadTableView {
+    NSInteger items = 0;
+    NSInteger sections = 0;
+    
+    if (self.tableView.dataSource && [self.tableView.dataSource respondsToSelector:@selector(numberOfSectionsInTableView:)]) {
+        sections = [self.tableView.dataSource numberOfSectionsInTableView:self.tableView];
+    }
+    
+    if (self.tableView.dataSource && [self.tableView.dataSource respondsToSelector:@selector(tableView:numberOfRowsInSection:)]) {
+        for (NSInteger section = 0; section < sections; section++) {
+            items += [self.tableView.dataSource tableView:self.tableView numberOfRowsInSection:section];
+        }
+    }
+    
+    
+    
+    // 添加为空时显示的View
+    if (items == 0 && sections == 0) {
+        [self.tableView addSubview:self.emptyView];
+        self.emptyView.hidden = NO;
+    } else {
+        [self.emptyView removeFromSuperview];
+        self.emptyView.hidden = YES;
+    }
+    
+    // 判断隐藏底部刷新控件
+    [self p_footerViewVisible:!self.hasNextPage];
+    
+
+    [self isHiddenFooterImgView];
+    
+    
+    
+    [self.tableView reloadData];
+}
+
+- (void)p_footerViewVisible:(BOOL)visible {
+    self.tableView.mj_footer.hidden = visible;
+}
+
+
+- (void)isHiddenFooterImgView {
+    
+    if (self.emptyView.hidden && !self.hasNextPage &&self.tableView.mj_footer) {
+        self.emptyFootView.hidden = NO;
+        self.tableView.tableFooterView = self.emptyFootView;
+    }else{
+        self.emptyFootView.hidden = YES;
+    }
+    
+    
+}
+
+- (void)dszGetNetWork {
+    
+}
+
+- (void)changeEmptyView:(UIView *)view {
+    _emptyView = view;
+    [self.tableView reloadData];
+}
+
+- (void)resetEmptyViewFrame:(CGRect)frame {
+    self.emptyView.frame = frame;
+}
+
+- (void)setEmptyViewHidden {
+    self.emptyView.hidden = YES;
+}
+
+
+@end
